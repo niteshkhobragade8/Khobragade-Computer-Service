11b8b13c28d93b3f4df50d250da6e8c28de2524b
niteshkhobragade8 <niteshkhobragade8@gmail.com>
2026-07-25 19:37:59 +0530

    Create services.js

---
+++ services.js
@@ -0,0 +1,57 @@
+import { db } from "./firebase-config.js";
+
+import {
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const saveService = document.getElementById("saveService");
+
+async function loadServices() {
+
+    const servicesList = document.getElementById("servicesList");
+
+    servicesList.innerHTML = "";
+
+    const snapshot = await getDocs(collection(db, "services"));
+
+    snapshot.forEach((doc) => {
+
+        const data = doc.data();
+
+        servicesList.innerHTML += `
+        <div class="card">
+            <h3>${data.icon} ${data.name}</h3>
+        </div>
+        `;
+
+    });
+
+}
+
+saveService.addEventListener("click", async () => {
+
+    const name = document.getElementById("serviceName").value;
+    const icon = document.getElementById("serviceIcon").value;
+
+    if(name===""){
+        alert("Enter Service Name");
+        return;
+    }
+
+    await addDoc(collection(db,"services"),{
+        name,
+        icon
+    });
+
+    alert("Service Saved");
+
+    document.getElementById("serviceName").value="";
+    document.getElementById("serviceIcon").value="";
+
+    loadServices();
+
+});
+
+loadServices();
