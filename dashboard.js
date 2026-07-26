eab6c547ff143726be652bd2febaa317fbfb787a
niteshkhobragade8 <niteshkhobragade8@gmail.com>
2026-07-26 14:57:22 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,86 @@
+import { db } from "./firebase-config.js";
 
+import {
+
+collection,
+getDocs
+
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+async function loadDashboard(){
+
+const updates=await getDocs(collection(db,"updates"));
+
+const services=await getDocs(collection(db,"services"));
+
+document.getElementById("totalUpdates").innerText=updates.size;
+
+document.getElementById("totalServices").innerText=services.size;
+
+}
+async function loadVisitors(){
+
+const visitors=localStorage.getItem("visitors") || 0;
+
+document.getElementById("totalVisitors").innerText=visitors;
+
+}
+
+function increaseVisitor(){
+
+let visitors=Number(localStorage.getItem("visitors") || 0);
+
+visitors++;
+
+localStorage.setItem("visitors",visitors);
+
+}
+
+increaseVisitor();
+window.refreshDashboard = async function(){
+
+await loadDashboard();
+
+await loadVisitors();
+
+};
+
+window.addEventListener("DOMContentLoaded", async () => {
+
+await loadDashboard();
+
+await loadVisitors();
+
+});
+window.resetDashboard = function(){
+
+document.getElementById("totalUpdates").innerText="0";
+
+document.getElementById("totalServices").innerText="0";
+
+document.getElementById("totalVisitors").innerText="0";
+
+};
+
+window.reloadDashboard = async function(){
+
+await loadDashboard();
+
+await loadVisitors();
+
+};
+window.addEventListener("load", async () => {
+
+await loadDashboard();
+
+await loadVisitors();
+
+});
+
+export {
+
+loadDashboard,
+
+loadVisitors
+
+};
