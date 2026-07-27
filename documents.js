e0bda0bfd0f90ad54ada02f56ea9c037f743428e
niteshkhobragade8 <niteshkhobragade8@gmail.com>
2026-07-27 15:50:56 +0530

    Add documents.js for document management features

---
+++ documents.js
@@ -0,0 +1,15 @@
+import { db } from "./firebase-config.js";
+
+console.log("Documents Manager Loaded");
+
+const uploadBtn = document.getElementById("uploadDocument");
+
+if (uploadBtn) {
+
+uploadBtn.addEventListener("click", () => {
+
+alert("Document Upload Feature Coming Soon");
+
+});
+
+}
