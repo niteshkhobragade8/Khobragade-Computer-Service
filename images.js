4c319b7c07fde6060137bdfc24eed59a6918b2fd
niteshkhobragade8 <niteshkhobragade8@gmail.com>
2026-07-28 21:56:26 +0530

    Update images.js

--- images.js
+++ images.js
@@ -1,25 +1,13 @@
+let allImages = [];
+
 async function loadImages() {
     const imagesList = document.getElementById("imagesList");
 
     try {
         const response = await fetch("./images/images.json");
-        const images = await response.json();
+        allImages = await response.json();
 
-        if (images.length === 0) {
-            imagesList.innerHTML = "<p>No Images Available</p>";
-            return;
-        }
-
-        imagesList.innerHTML = "";
-
-        images.forEach(img => {
-            imagesList.innerHTML += `
-                <div class="image-card">
-                    <img src="${img.url}" alt="${img.name}" width="180">
-                    <p>${img.name}</p>
-                </div>
-            `;
-        });
+        displayImages(allImages);
 
     } catch (e) {
         imagesList.innerHTML = "<p>Failed to load images.</p>";
@@ -27,4 +15,36 @@ async function loadImages() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", loadImages);
+function displayImages(images) {
+    const imagesList = document.getElementById("imagesList");
+
+    if (images.length === 0) {
+        imagesList.innerHTML = "<p>No Images Available</p>";
+        return;
+    }
+
+    imagesList.innerHTML = "";
+
+    images.forEach(img => {
+        imagesList.innerHTML += `
+            <div class="image-card">
+                <img src="${img.url}" alt="${img.name}">
+                <p>${img.name}</p>
+            </div>
+        `;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadImages();
+
+    document.getElementById("searchImage").addEventListener("input", function () {
+        const text = this.value.toLowerCase();
+
+        const filtered = allImages.filter(img =>
+            img.name.toLowerCase().includes(text)
+        );
+
+        displayImages(filtered);
+    });
+});
