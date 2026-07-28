751371baa3c5fcf15d54656529e27d1a56070c08
niteshkhobragade8 <niteshkhobragade8@gmail.com>
2026-07-28 19:15:43 +0530

    Update images.js

--- images.js
+++ images.js
@@ -1,7 +1,24 @@
 const uploadBtn = document.getElementById("uploadImage");
+const imageInput = document.getElementById("imageUpload");
+const imagesList = document.getElementById("imagesList");
 
 if (uploadBtn) {
   uploadBtn.addEventListener("click", () => {
-    alert("Image Upload Module Ready");
+
+    if (!imageInput.files.length) {
+      alert("Please select an image.");
+      return;
+    }
+
+    const file = imageInput.files[0];
+
+    imagesList.innerHTML = `
+      <div class="card">
+        <img src="./images/${file.name}"
+        style="width:100%;max-width:300px;border-radius:10px;">
+        <h3>${file.name}</h3>
+      </div>
+    `;
+
   });
 }
