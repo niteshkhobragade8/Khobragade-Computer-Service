2dd01e0bc99a5c73708b4b06feb434fb0b83cb4b
niteshkhobragade8 <niteshkhobragade8@gmail.com>
2026-07-30 10:09:31 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,24 +4,18 @@ const pages = document.querySelectorAll(".page");
 function openPage(pageName) {
 
     pages.forEach(page => {
-
         page.classList.remove("active");
-
     });
 
     menuItems.forEach(item => {
-
         item.classList.remove("active");
-
     });
 
     const targetPage =
         document.getElementById(pageName + "Page");
 
     if (targetPage) {
-
         targetPage.classList.add("active");
-
     }
 
     const activeMenu =
@@ -30,10 +24,11 @@ function openPage(pageName) {
         );
 
     if (activeMenu) {
-
         activeMenu.classList.add("active");
-
     }
+
+    // Last Open Page Save
+    localStorage.setItem("activePage", pageName);
 
 }
 
@@ -42,10 +37,7 @@ menuItems.forEach(item => {
     item.addEventListener("click", () => {
 
         if (item.id === "logoutBtn") {
-
-            // Logout dashboard.js handle karega
             return;
-
         }
 
         const page = item.dataset.page;
@@ -55,6 +47,20 @@ menuItems.forEach(item => {
         openPage(page);
 
     });
+
+});
+
+// Page Refresh ke baad wahi page open hoga
+window.addEventListener("DOMContentLoaded", () => {
+
+    const savedPage =
+        localStorage.getItem("activePage");
+
+    if (savedPage) {
+        openPage(savedPage);
+    } else {
+        openPage("dashboard");
+    }
 
 });
 
